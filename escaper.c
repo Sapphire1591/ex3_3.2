e619577dd2fc528e9381292cc354f69a69f9814a
moranhait <moranh@campus.technion.ac.il>
2017-06-01 13:39:08 +0300

    added escaper

--- escaper.c
+++ escaper.c
@@ -1,285 +1,74 @@
-#include <stdio.h>
+#include "escaper.h"
+#include "set.h"
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
+#include <stdio.h>
+#include <malloc.h>
 
-#include "list_mtm1.h"
 
-typedef struct list_node {
-    ListElement data;
-    struct list_node* next;
-}*Node;
-
-struct list_t {
-    Node head;
-    Node iterator;
-    CopyListElement copy;
-    FreeListElement free;
+struct escaper_struct {
+    char* email;
+    TechnionFaculty faculty;
+    int skill_level;
+    Order *first_order;
 };
 
-List listCreate(CopyListElement copyElement, FreeListElement freeElement){
-    if(!copyElement || !freeElement){
+Escaper escaperCreate(char* email, TechnionFaculty faculty, int skill_level){
+    //need to check that all parameters are valid
+    Escaper escaper = malloc(sizeof(*escaper));
+    if(!escaper){
         return NULL;
     }
-    List list = malloc(sizeof(struct list_t));
-    if (!list) {
-        return NULL;
-    }
-    list->head = malloc(sizeof(struct list_node));
-    if (!list->head) {
-        return NULL;
-    }
-    list->head->data = NULL;
-    list->head->next = NULL;
-    list->iterator = malloc(sizeof(struct list_node));
-    if (!list->iterator) {
-        return NULL;
-    }
-    list->iterator = list->head->next;
-    list->copy = copyElement;
-    list->free = freeElement;
-    return list;
+    escaper->email = email;
+    escaper->faculty = faculty;
+    escaper->skill_level = skill_level;
+    return escaper;
 }
 
-List listCopy(List list) {
-    if(!list){
-        return NULL;
-    }
-    List new_list = listCreate(list->copy, list->free);
-    if (!new_list) {
-        return NULL;
-    }
-    Node element = list->head->next;
-    Node new_element = new_list->head->next;
-    while (element) {
-        new_element = list->copy(element);
-        element = element->next;
-        new_element = new_element->next;
-    }
-    new_list->iterator = list->iterator;
-    return new_list;
+Set escaperSetCreate(copySetElements copyElement, freeSetElements freeElement, compareSetElements compareElements) {
+    Set escaper_set = setCreate(copyElement, freeElement, compareElements);
+    return escaper_set;
 }
 
-int listGetSize(List list) {
-    if(!list){
-        return -1;
-    }
-    int counter = 0;
-    Node current = list->head->next;
-    while (current) {
-        counter++;
-        current = current->next;
-    }
-    return counter;
+void escaperSetDestroy(Set set){
+    setDestroy(set);
 }
 
-ListElement listGetFirst(List list) {
-    if(!list){
-        return NULL;
-    }
-    if (list->head->next) {
-        list->iterator = list->head->next;
-        return list->head->next->data;
-    }
-    return NULL; //list is empty
+int escaperSetGetSize(Set set){
+    int size = setGetSize(set);
+    return size;
 }
 
-ListElement listGetNext(List list) {
-    if(!list){
-        return NULL;
-    }
-    Node current = listGetCurrent(list);
-    if (!current || !current->next) {
-        return NULL;
-    }
-    list->iterator = current->next;
-    return current->next->data;
+bool escaperSetIsIn(Set set, Escaper escaper){
+    bool is_in = setIsIn(set, escaper);
+    return is_in;
 }
 
-ListElement listGetCurrent(List list) {
-    if(!list){
-        return NULL;
-    }
-    return list->iterator->data;
+Escaper escaperSetGetFirst(Set set){
+    Escaper escaper = setGetFirst(set);
+    return escaper;
 }
 
-ListResult listInsertFirst(List list, ListElement element) {
-    if(!list || !element){
-        LIST_NULL_ARGUMENT;
-    }
-    Node new_node = malloc(sizeof(struct list_node));
-    if (!new_node) {
-        return LIST_OUT_OF_MEMORY;
-    }
-    new_node->data = element;
-    new_node->next = list->head->next;
-    list->head->next = new_node;
-    return LIST_SUCCESS;
+Escaper escaperSetGetNext(Set set){
+    Escaper escaper = setGetNext(set);
+    return escaper;
 }
 
-ListResult listInsertLast(List list, ListElement element) {
-    if(!list || !element){
-        LIST_NULL_ARGUMENT;
-    }
-    Node new_node = malloc(sizeof(struct list_node));
-    if (!new_node) {
-        return LIST_OUT_OF_MEMORY;
-    }
-    Node last_node = list->head->next;
-    if (last_node == NULL) {
-        new_node->data = element;
-        new_node->next = NULL;
-        list->head->next = new_node;
-        return LIST_SUCCESS;
-    }
-    else
-        while (last_node->next) { //finds the "next" that points to NULL
-            last_node = last_node->next;
-        }
-    new_node->data = element;
-    new_node->next = NULL;
-    last_node->next = new_node;
-    return LIST_SUCCESS;
+Escaper escaperSetGetCurrent(Set set){
+    Escaper escaper = setGetCurrent(set);
+    return escaper;
 }
 
-ListResult listInsertBeforeCurrent(List list, ListElement element) {
-    if(list || element){
-        LIST_NULL_ARGUMENT;
-    }
-    Node new_node = malloc(sizeof(struct list_node));
-    if (!new_node) {
-        return LIST_OUT_OF_MEMORY;
-    }
-    Node before_current = list->head->next;
-    //find the "next" that points to current:
-    while (before_current->next != list->iterator) {
-        before_current = before_current->next;
-    }
-    new_node->data = element;
-    new_node->next = before_current->next;
-    before_current->next = new_node;
-    return LIST_SUCCESS;
+SetResult escaperSetAdd(Set set, Escaper escaper){
+    SetResult result = setAdd(set, escaper);
+    return result;
 }
 
-ListResult listInsertAfterCurrent(List list, ListElement element) {
-    if(!list && !element){
-        LIST_NULL_ARGUMENT;
-    }
-    if(!listGetCurrent(list)){
-        LIST_INVALID_CURRENT;
-    }
-    Node new_node = malloc(sizeof(struct list_node));
-    if (!new_node) {
-        return LIST_OUT_OF_MEMORY;
-    }
-    new_node->data = element;
-    new_node->next = list->iterator->next;
-    list->iterator->next = new_node;
-    return LIST_SUCCESS;
+SetResult escaperSetRemove(Set set, Escaper escaper){
+    SetResult result = setRemove(set, escaper);
+    return result;
 }
 
-
-ListResult listRemoveCurrent(List list){
-    if(!list){
-        LIST_NULL_ARGUMENT;
-    }
-    if(!listGetCurrent(list)){
-        LIST_INVALID_CURRENT;
-    }
-    Node next = list->iterator->next;
-    listInsertBeforeCurrent(list, next);
-    list->free(list->iterator->data);
-    free(list->iterator);
-    list->iterator = NULL;
-    return LIST_SUCCESS;
-}
-
-static void swap(Node a, Node b){
-    ListElement temp = a->data;
-    a->data = b->data;
-    b->data = temp;
-}
-
-ListResult listSort(List list, CompareListElements compareElement){
-    if(!list || !compareElement){
-        return LIST_NULL_ARGUMENT;
-    }
-    Node ptr = NULL; //pointer to the compared element
-    list->iterator = listGetFirst(list);
-    if(list->iterator){
-        ptr = list->iterator->next;
-    }
-    while(list->iterator){
-        while(ptr){
-            if(compareElement(ptr, list->iterator) > 0){
-                swap(ptr, list->iterator);
-            }
-            else{
-                ptr = ptr->next;
-            }
-        }
-    }
-    return LIST_SUCCESS;
-}
-
-/*
-    //bubble sort:
-    bool sorted = false; //signs that the list is sorted
-    list->iterator = listGetFirst(list);
-    Node ptr = NULL; //ptr to the compared element
-
-    while(!list->iterator){
-        ptr = list->iterator->next;
-        else if(compareElement(list->iterator, ptr)){ //what if ptr==NULL?
-            listInsertBeforeCurrent(list, ptr);
-            ptr = list->iterator->next;
-        }
-        else{
-            list->iterator = list->iterator->next;
-        }
-    }
-}
-*/
-
-List listFilter(List list, FilterListElement filterElement, ListFilterKey key) {
-    if(!list){
-        return NULL;
-    }
-    List new_list = listCreate(list->copy, list->free);
-    if(!new_list){
-        return NULL;
-    }
-    Node ptr = list->head->next;
-    if (!ptr) {
-        return NULL;
-    }
-    while(ptr){
-        if(filterElement(ptr->data, key)) {
-            listInsertLast(new_list, ptr->data);
-        }
-        ptr = ptr->next;
-    }
-    new_list->iterator = new_list->head->next;
-    return new_list;
-}
-
-ListResult listClear(List list){
-    if(list){
-        return LIST_NULL_ARGUMENT;
-    }
-    Node ptr;
-    while(!list->head->next){
-        ptr = list->head->next;
-        list->free(ptr->data);
-        ptr = ptr->next;
-        free(list->head->next);
-        list->head->next = ptr;
-    }
-    return LIST_SUCCESS;
-}
-
-void listDestroy(List list) {
-    assert(list);
-    listClear(list);
-    free(list);
+SetResult escaperSetClear(Set set) {
+    SetResult result = escaperSetClear(set);
+    return result;
 }
